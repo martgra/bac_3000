2aeea838a793d42c7b8837ce3add64bfb580b223
Martin Gran <martgra@gmail.com>
2017-02-14 10:39:11 +0100

    har laget nedlastingmetode for notMNIST

---
+++ notMNIST/notMNIST_read.py
@@ -0,0 +1,76 @@
+import os
+import sys
+import tarfile
+from urllib.request import urlretrieve
+import pickle as pickle
+
+url = 'http://commondatastorage.googleapis.com/books1000/'
+last_percent_reported = None
+data_root = './notMNIST'
+num_classes = 10
+
+
+def download_hook(count, blockSize, totalSize):
+    """A hook to report the progress of a download. This is mostly intended for users with
+    slow internet connections. Reports every 5% change in download progress.
+    """
+    global last_percent_reported
+    percent = int(count * blockSize * 100 / totalSize)
+
+    if last_percent_reported != percent:
+        if percent % 5 == 0:
+            sys.stdout.write("%s%%" % percent)
+            sys.stdout.flush()
+        else:
+            sys.stdout.write(".")
+            sys.stdout.flush()
+
+        last_percent_reported = percent
+
+
+def download_notmnist(filename, expected_bytes, force=False):
+    dest_filename = os.path.join(data_root, filename)
+    if force or not os.path.exists(dest_filename):
+        print('Attempting to download:', filename)
+        filename, _ = urlretrieve(url + filename, dest_filename, reporthook=download_hook)
+        print('\nDownload Complete!')
+    statinfo = os.stat(dest_filename)
+    if statinfo.st_size == expected_bytes:
+        print('Found and verified', dest_filename)
+    else:
+        raise Exception('Failed to verify' + dest_filename + '. Can you get to it with a browser?')
+    return dest_filename
+
+
+def extract_notmnist(filename, force=False):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    if os.path.isdir(root) and not force:
+        # You may override by setting force=True.
+        print('%s already present - Skipping extraction of %s.' % (root, filename))
+    else:
+        print('Extracting data for %s. This may take a while. Please wait.' % root)
+        tar = tarfile.open(filename)
+        sys.stdout.flush()
+        tar.extractall(data_root)
+        tar.close()
+    data_folders = [
+        os.path.join(root, d) for d in sorted(os.listdir(root))
+        if os.path.isdir(os.path.join(root, d))]
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+    print(data_folders)
+    return data_folders
+
+train_filename = download_notmnist('notMNIST_large.tar.gz', 247336696)
+test_filename = download_notmnist('notMNIST_small.tar.gz', 8458043)
+
+trained_folders = extract_notmnist(train_filename)
+test_folders = extract_notmnist(test_filename)
+
+
+
+
+
+
