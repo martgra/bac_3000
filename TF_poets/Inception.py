8cf8edbcbe0396de1d1952585c49eec54ebe14ad
Martin Gran <martgra@gmail.com>
2017-03-14 20:24:09 +0100

    Sprer trening på gpu og augmentation på GPU

--- TF_poets/Inception.py
+++ TF_poets/Inception.py
@@ -5,6 +5,7 @@ from keras.models import Model
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras import backend as K
 from keras.optimizers import SGD, Adam
+from keras import callbacks
 
 
 K.set_image_dim_ordering('tf')
@@ -14,10 +15,16 @@ val_dir = '/home/jason/train/validate'
 img_size = 299
 gen_batch = 64
 classes = 9
-weights_path = '/home/jason/train'
+weights_path = '/home/jason/train/train_weights'
 epoker = 50
 sdg = SGD(lr=0.0001, momentum=0.9)
+adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=0.1, decay=0.5)
 samples = 20
+log = '/home/jason/train'
+save_path = '/home/jason/train'
+tensorboard = callbacks.TensorBoard(log_dir=log, histogram_freq=1, write_graph=False)
+drop = 0.5
+
 
 
 # create the base pre-trained model
@@ -29,7 +36,7 @@ x = GlobalAveragePooling2D()(x)
 # let's add a fully-connected layer
 x = Dense(1024, activation='relu')(x)
 # and a logistic layer -- let's say we have 200 classes
-x = Dropout(0.5)(x)
+x = Dropout(drop)(x)
 predictions = Dense(classes, activation='softmax')(x)
 
 # this is the model we will train
@@ -41,7 +48,6 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='rmsprop', loss='categorical_crossentropy',  metrics=['accuracy'])
 
 # train the model on the new data for a few epochs
 
@@ -58,36 +64,40 @@ train_data = image.ImageDataGenerator(
                         )
 
 #husk å ikke glemme å gjøre rescale på begge a?
-test_datagen = image.ImageDataGenerator(
-                            rescale=1. / 255,
-                            dim_ordering=K.image_dim_ordering()
-                        )
 
-validation_generator = test_datagen.flow_from_directory(
-                            val_dir,
-                            target_size=(img_size, img_size),
-                            batch_size=gen_batch,
-                            class_mode='categorical',
-                            classes=['Agaricus_bisporus', 'Amanita_muscaria', 'Amanita_virosa', 'Boletus_edulis', 'Cantharellus_cibarius', 'Cortinarius_rubellus', 'Craterellus_cornucopioides', 'Lactarius_deterrimus', 'Suillus_variegatus'],
-                            shuffle='False'
-                        )
+with tf.device('/cpu:0'):
+    test_datagen = image.ImageDataGenerator(
+                                rescale=1. / 255,
+                                dim_ordering=K.image_dim_ordering()
+                            )
 
-train_generator = train_data.flow_from_directory(
-                            train_dir,
-                            target_size=(img_size, img_size),
-                            batch_size=gen_batch,
-                            shuffle='False',
-                            class_mode='categorical',
-                            classes=['Agaricus_bisporus', 'Amanita_muscaria', 'Amanita_virosa', 'Boletus_edulis', 'Cantharellus_cibarius', 'Cortinarius_rubellus', 'Craterellus_cornucopioides', 'Lactarius_deterrimus', 'Suillus_variegatus'],
-                        )
+    validation_generator = test_datagen.flow_from_directory(
+                                val_dir,
+                                target_size=(img_size, img_size),
+                                batch_size=gen_batch,
+                                class_mode='categorical',
+                                classes=['Agaricus_bisporus', 'Amanita_muscaria', 'Amanita_virosa', 'Boletus_edulis', 'Cantharellus_cibarius', 'Cortinarius_rubellus', 'Craterellus_cornucopioides', 'Lactarius_deterrimus', 'Suillus_variegatus'],
+                                shuffle='False'
+                            )
 
-model.fit_generator(
-                            train_generator,
-                            samples_per_epoch=128 * samples,
-                            nb_epoch=epoker,
-                            validation_data=validation_generator,
-                            nb_val_samples= 32 * samples
-                    )
+    train_generator = train_data.flow_from_directory(
+                                train_dir,
+                                target_size=(img_size, img_size),
+                                batch_size=gen_batch,
+                                shuffle='False',
+                                class_mode='categorical',
+                                classes=['Agaricus_bisporus', 'Amanita_muscaria', 'Amanita_virosa', 'Boletus_edulis', 'Cantharellus_cibarius', 'Cortinarius_rubellus', 'Craterellus_cornucopioides', 'Lactarius_deterrimus', 'Suillus_variegatus'],
+                            )
+with tf.device('/gpu:0'):
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy',  metrics=['accuracy'])
+    model.fit_generator(
+                                train_generator,
+                                samples_per_epoch=128 * samples,
+                                nb_epoch=epoker,
+                                validation_data=validation_generator,
+                                nb_val_samples=32,
+                                callbacks=[tensorboard]
+                        )
 
 
 # at this point, the top layers are well trained and we can start fine-tuning
@@ -108,16 +118,28 @@ for layer in model.layers[172:]:
 
 # we need to recompile the model for these modifications to take effect
 # we use SGD with a low learning rate
+with tf.device('/gpu:0'):
+    model.compile(optimizer=sdg, loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.compile(optimizer=sdg, loss='categorical_crossentropy', metrics=['accuracy'])
+    # we train our model again (this time fine-tuning the top 2 inception blocks
+    # alongside the top Dense layers
 
-# we train our model again (this time fine-tuning the top 2 inception blocks
-# alongside the top Dense layers
+    model.fit_generator(
+                                train_generator,
+                                samples_per_epoch=128 * samples,
+                                nb_epoch=epoker,
+                                validation_data=validation_generator,
+                                nb_val_samples=32
+                            )
+model.save(save_path)
 
-model.fit_generator(
-                            train_generator,
-                            samples_per_epoch=128 * samples,
-                            nb_epoch=epoker,
-                            validation_data=validation_generator,
-                            nb_val_samples=32 * samples
+test_datagen2 = image.ImageDataGenerator(rescale=1. / 255, dim_ordering=K.image_dim_ordering())
+
+prediction_generator = test_datagen2.flow_from_directory(
+                                val_dir, target_size=(img_size, img_size),
+                                batch_size=gen_batch,
+                                classes=['Agaricus_bisporus', 'Amanita_muscaria', 'Amanita_virosa', 'Boletus_edulis', 'Cantharellus_cibarius',
+                                            'Cortinarius_rubellus', 'Craterellus_cornucopioides', 'Lactarius_deterrimus', 'Suillus_variegatus'],
+                                shuffle='False'
                         )
+model.predict_generator(prediction_generator)
